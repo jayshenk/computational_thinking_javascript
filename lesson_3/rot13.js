3eab9f937917137c8941ea8dad7e2ddf0a6c1fdd
Jay Shenk <jay.shenk@gmail.com>
2016-09-02 11:02:59 -0400

    Initial solution for rot13 cipher

---
+++ lesson_3/rot13.js
@@ -0,0 +1,32 @@
+function rot13(string) {
+  var ciphered = '';
+  var charCode;
+
+  for (var i = 0; i < string.length; i++) {
+    charCode = string.charCodeAt(i);
+
+    // Capital letters
+    if (charCode >= 65 && charCode < 78) {
+      charCode += 13;
+    } else if (charCode >= 78 && charCode <= 90) {
+      charCode -= 13;
+    }
+
+    // Lowercase letters
+    if (charCode >= 97 && charCode < 110) {
+      charCode += 13;
+    } else if (charCode >= 110 && charCode <= 122) {
+      charCode -= 13;
+    }
+
+    ciphered += String.fromCharCode(charCode);
+  }
+
+  return ciphered;
+}
+
+console.log(rot13('Teachers open the door, but you must enter by yourself.'));
+// Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.
+
+console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
+// Teachers open the door, but you must enter by yourself.
