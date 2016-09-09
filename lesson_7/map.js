0c3dfb8dcdb92a9329338f5eb7ac117f934a2c43
Jay Shenk <jay.shenk@gmail.com>
2016-09-09 17:46:53 -0400

    Complete myMap function

---
+++ lesson_7/map.js
@@ -0,0 +1,13 @@
+function myMap(array, func) {
+  var result = [];
+
+  array.forEach(function(element) {
+    result.push(func(element));
+  });
+
+  return result;
+}
+
+var plusOne = function(n) { return n + 1; };
+
+myMap([1, 2, 3, 4], plusOne);
